9850c50433739410236a40356ca2f2e96507fd90
XxDragonWestxX <112690686+XxDragonWestxX@users.noreply.github.com>
2024-01-16 02:18:22 +0300

    Finish navbar HTML and CSS. Added first content container

---
+++ JS/slider-courusel.js
@@ -0,0 +1,11 @@
+const slider = document.getElementsByClassName('.slider')
+// const slides_count = slides.length;
+
+let slide_index = 0;
+
+function selectNextCard()
+{
+    slider.sty
+}
+
+selectNextCard();
