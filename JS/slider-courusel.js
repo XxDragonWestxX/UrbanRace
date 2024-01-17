62270dfd161b2b8aba0b14a3ba0948a94d4f4294
XxDragonWestxX <112690686+XxDragonWestxX@users.noreply.github.com>
2024-01-17 19:02:51 +0300

    Full adaptive company presentation

--- JS/slider-courusel.js
+++ JS/slider-courusel.js
@@ -7,14 +7,21 @@ const slides_count = slides.length
 
 let slide_index = 0
 let next_slide = 0
+let prev_slide = slides.length - 1
 
 function nextCard()
 {   
+    slides[prev_slide].classList.remove('prev')
+
     next_slide = slide_index + 1
     if (next_slide === slides_count) next_slide = 0
 
     slides[slide_index].classList.remove('select')
+    slides[slide_index].classList.add('prev')
+    prev_slide = slide_index
+
     slides[next_slide].classList.add('select')
+
     viewer_points[slide_index].classList.remove('select')
     viewer_points[next_slide].classList.add('select')
 
