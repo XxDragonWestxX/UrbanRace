a0a5f36884c900f98cdaabe7a4a5eb7be6cae849
XxDragonWestxX <112690686+XxDragonWestxX@users.noreply.github.com>
2024-01-18 01:17:01 +0300

    Added JS for dropdown-menu

---
+++ JS/dropdown-menu.js
@@ -0,0 +1,25 @@
+$('.selector').click(function()
+{
+    $(this).toggleClass('active')
+})
+
+$('.selector .items-list .item').click(function()
+{
+    $(this).parent().parent().children('.select-item').children('.value').html($(this).html() + '<div class="clear"></div>')
+    $(this).parent().parent().attr('value', $(this).attr('value'))
+    $(this).parent().children('.select').removeClass('select')
+    $(this).addClass('select')
+})
+
+$('.selector .clear').click(function()
+{
+    $(this).parent().parent().attr('value', '#')
+    $(this).parent().parent().attr('value', '#')
+    $(this).parent().children('.value').html('')
+    $(this).parent().parent().children('.items-list').children('.item').removeClass('select')
+})
+
+$('.blackout-screen').click(function()
+{
+    $('.selector.active').toggleClass('active')
+})
