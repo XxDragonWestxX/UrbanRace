d5332c691376f995a589b680e70d8dd8df53c837
XxDragonWestxX <xxdragonwestxx@gmail.com>
2024-01-18 14:20:14 +0300

    Added adaptive and JS for 'selection by car' conteiner

--- JS/dropdown-menu.js
+++ JS/dropdown-menu.js
@@ -1,12 +1,13 @@
-$('.selector').click(function()
+$('.selector .value').click(function()
 {
-    $(this).toggleClass('active')
+    $(this).parent().parent().toggleClass('active')
 })
 
 $('.selector .items-list .item').click(function()
 {
     $(this).parent().parent().children('.select-item').children('.value').html($(this).html() + '<div class="clear"></div>')
     $(this).parent().parent().attr('value', $(this).attr('value'))
+    $(this).parent().parent().removeClass('active')
     $(this).parent().children('.select').removeClass('select')
     $(this).addClass('select')
 })
@@ -14,12 +15,12 @@ $('.selector .items-list .item').click(function()
 $('.selector .clear').click(function()
 {
     $(this).parent().parent().attr('value', '#')
-    $(this).parent().parent().attr('value', '#')
-    $(this).parent().children('.value').html('')
+    $(this).parent().parent().removeClass('active')
+    $(this).parent().children('.value').html($(this).parent().parent().attr('placeholder'))
     $(this).parent().parent().children('.items-list').children('.item').removeClass('select')
 })
 
 $('.blackout-screen').click(function()
 {
-    $('.selector.active').toggleClass('active')
+    $('.selector.active').removeClass('active')
 })
