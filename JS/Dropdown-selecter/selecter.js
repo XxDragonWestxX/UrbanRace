1a93a299fb0ba23e4b9d2669159a1d1ae7a59c28
XxDragonWestxX <xxdragonwestxx@gmail.com>
2024-01-31 16:22:40 +0300

    Filters js and css

--- JS/Dropdown-selecter/selecter.js
+++ JS/Dropdown-selecter/selecter.js
@@ -1,5 +1,17 @@
-$('.item').click(function()
-{
-    console.log("by")
-    $(this).parent().parent('.dropdown').child('.lounch-container').child('.louncher').removeAttr('checked')
+
+$('.dropdown .item').click(function () {
+    $(this).parent().parent().children('.lounch-container').children('.louncher').prop('checked', false)
+    $(this).parent().parent().children('.lounch-container').children('.select-container').children('.select').html($(this).html())
+    $(this).parent().children('.select').removeClass('select')
+    $(this).addClass('select')
+})
+
+$('.dropdown .blackscreen').click(function () {
+    $(this).parent().children('.lounch-container').children('.louncher').prop('checked', false)
+})
+
+$('.dropdown .clear').click(function () {
+    $(this).parent().children('.select').html('')
+    $(this).parent().parent().children('.louncher').prop('checked', false)
+    $(this).parent().parent().parent().children('.item-list').children('.select').removeClass('select')
 })
